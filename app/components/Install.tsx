b6f6189cf1aee6b5c738101995e29ac51fb64751
VMASPAD <tomaseavila@gmail.com>
2025-01-21 00:10:50 -0300

    v1.1

--- app/components/Install.tsx
+++ app/components/Install.tsx
@@ -3,7 +3,7 @@ import CopyBlock from './CopyBlock'
 function Install() {
   return (
     <div>
-      <CopyBlock text="1" />
+      <CopyBlock text="npx shadcn@latest add " />
       <p className='my-5 font-bold'>Dependencies:</p>
       <CopyBlock text="npm i react-dnd react-dnd-html5-backend" />
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Install from "./components/Install";
 import Usage from "./components/Usage";
 import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
+import GridPattern from "@/components/ui/grid-pattern";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
   const [position, setPosition] = useState("style1");
@@ -33,6 +35,26 @@ export default function Home() {
       </section>
       <br />
       <Footer />
+      <GridPattern
+        squares={[
+          [4, 4],
+          [5, 1],
+          [8, 2],
+          [5, 3],
+          [5, 5],
+          [10, 10],
+          [12, 15],
+          [15, 10],
+          [10, 15],
+          [15, 10],
+          [10, 15],
+          [15, 10],
+        ]}
+        className={cn(
+          "[mask-image:radial-gradient(400px_circle_at_center,white,transparent)]",
+          "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
+        )}
+      />
     </>
   );
 }
